d40cb5ad83af13ef2c5b49ed5274a8b44ce4d29f
tugbaesat <tugbaesat007@gmail.com>
2022-11-27 16:50:23 +0300

    create a diceRoll function and use that

--- Dicee Challenge/index.js
+++ Dicee Challenge/index.js
@@ -1,29 +1,29 @@
-var randomNumber1 = Math.random();
+function diceRoll() {
+    var randomNumber = Math.random();
+    
+    randomNumber = randomNumber * 6;
+    
+    
+    randomNumber = Math.floor(randomNumber) + 1;
 
-randomNumber1 = randomNumber1 * 6;
+    return randomNumber;
+}
 
-randomNumber1 = Math.floor(randomNumber1) + 1;
+var randomNumber1 = diceRoll();
+document
+  .querySelector(".img1")
+  .setAttribute("src", "images/dice" + randomNumber1 + ".png");
 
-document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+var randomNumber2 = diceRoll();
 
-var randomNumber2 = Math.random();
-
-randomNumber2 = randomNumber2 * 6;
-
-randomNumber2 = Math.floor(randomNumber2) + 1;
-
-document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
-
-
+document
+  .querySelector(".img2")
+  .setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
 if (randomNumber2 > randomNumber1) {
-
-    document.querySelector("h1").innerHTML = "Player 2 Wins!"
-    
-}
-else if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML= "Player 1 Wins!"
-}
-else  {
-    document.querySelector("h1").innerHTML= "Dostluk Wins!"
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else {
+  document.querySelector("h1").innerHTML = "Dostluk Wins!";
 }
